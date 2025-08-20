75655556ed93092e81e349d8c0cadf12943bd75c
Felix Cederblad <felix_cederblad@hotmail.com>
2025-08-20 10:55:21 +0200

    Meta section props

--- react-media-player/src/components/MetaSection.tsx
+++ react-media-player/src/components/MetaSection.tsx
@@ -1,17 +1,18 @@
 import type { ReactElement } from "react";
+import type { ISong } from "../types";
 
-type SongMeta = {
-  artist: string;
-  songName: string;
-};
+interface IMetaSectionProps {
+  song: ISong;
+}
 
-export const MetaSection = (props: SongMeta): ReactElement => {
+export const MetaSection = ({ song }: IMetaSectionProps): ReactElement => {
+  const { artist, songName } = song;
   return (
     <div className="meta-section">
       <span className="material-symbols-outlined">add_circle</span>
       <div className="songname-artist-player-container">
-        <h4>{props.artist}</h4>
-        <p>{props.songName}</p>
+        <h4>{artist}</h4>
+        <p>{songName}</p>
       </div>
       <span className="material-symbols-outlined">heart_plus</span>
     </div>
--- react-media-player/src/components/Player.tsx
+++ react-media-player/src/components/Player.tsx
@@ -3,12 +3,13 @@ import SongImage from "../assets/song-image.png";
 import { MetaSection } from "./MetaSection";
 import { TimeSlider } from "./TimeSlider";
 import { MediaPlayerButtons } from "./MediaPlayerButtons";
+import { songs } from "../data";
 
 export const Player = (): ReactElement => {
   return (
     <div className="player-container">
       <img src={SongImage} className="player-image" alt="Song Cover" />
-      <MetaSection artist="A Rocket to the moon" songName="Like we used to" />
+      <MetaSection song={songs[0]} />
       <TimeSlider startTime="0:40" endTime="3:42" />
       <MediaPlayerButtons />
     </div>
